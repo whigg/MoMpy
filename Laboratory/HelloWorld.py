72ded9695be9d7099ea5bf827760095e5f2749a3
Palash Patole <palashpatole16@gmail.com>
2019-08-24 10:17:14 +0200

    1016 on 24 Aug 19

---
+++ Laboratory/HelloWorld.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Code by: Palash Patole
+MoMpy Project: Laboratory
+Date of creation: 24 August 2019
+
+Description:
+    First script in Python. Printing "Hello World". 
+    Includes definition of a function and calling it.
+    Includes experiments with user input, 
+    various variable types and simple algebra. 
+    
+
+"""
+
+# this is a comment line
+## how different is this from using single '#'?
+
+
+print ("Hello World !") # this is adding a comment on the side of a statement
+
+###################################
+# defining a function
+
+def cohle():
+    print ("Death created time to grow the things it would kill...")
+    
+
+cohle()
+# function ends when indentation is stopped
+###################################
+
+    
