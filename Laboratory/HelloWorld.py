0e46f039f40c083a992809e67c066bf1ba6e656a
Palash Patole <palashpatole16@gmail.com>
2019-08-24 17:56:43 +0200

    1756 on Aug 24, 2019

--- Laboratory/HelloWorld.py
+++ Laboratory/HelloWorld.py
@@ -24,10 +24,40 @@ print ("Hello World !") # this is adding a comment on the side of a statement
 
 def cohle():
     print ("Death created time to grow the things it would kill...")
-    
+   
+# function ends when indentation is stopped
 
 cohle()
-# function ends when indentation is stopped
 ###################################
+### simple algebric calculations
 
+import math
+
+print ("To solve the quadratic eqn: ax^2 + bx + c = 0:")
+
+a = input("Enter the value of a:")
+b = input("Enter the value of b:")
+c = input("Enter the value of c:")
+ 
+D = b**2 - 4*a*c #problem with this statement
+
+if D <0:
+
+    print ("This equation has no solutions")
     
+else:    
+    x1 = (-b - math.sqrt(D)) / (2*a)
+    x2 = (-b + math.sqrt(D)) / (2*a)
+    
+    print ("x1 = "),x1
+    print ("x2 = "),x2
+
+
+
+
+
+
+
+
+
+   
