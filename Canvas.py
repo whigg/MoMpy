1f641b4eca44862a19d308dc81c3e77707804546
Palash Patole <palashpatole16@gmail.com>
2019-11-19 21:40:54 +0100

    After ideas sheet updated, pygmo installed.

--- Canvas.py
+++ Canvas.py
@@ -10,22 +10,37 @@ Description:
     1. Generic canvas to test different modules
 """
 
-#from Modules.BasicAstrodynamics import convertCartesianToKepler
-#from Modules.BasicAstrodynamics import convertKeplerToCartesian
+from Modules.BasicAstrodynamics import convertCartesianToKepler
+from Modules.BasicAstrodynamics import convertKeplerToCartesian
 
 ## following method to import all the functions from a module is not recommended 
 ## in the cheat sheets
-from Modules.BasicAstrodynamics import * 
+#from Modules.BasicAstrodynamics import * 
 
 import math # to use value of Pi
 import numpy as np #NumPy library
+
+import spiceypy as spice
+
+import pygmo
 
 
 
 # Part(Problem) 1 of Basics-I assignment 
 S_bar = np.array([8751268.4691, -7041314.6869, 4846546.9938, 332.2601039, -2977.0815768, -4869.8462227]) 
 
-mu = 398600.441E9 # Gravitational parameter for Earth [m^3/s^2]
+
+# Extracting mu from SPICE kernels
+print (spiceypy.tkvrsn('TOOLKIT'))
+
+spice.furnsh("./External_files/Spice_kernels/kernel_load.txt")
+
+muE = spice.bodvrd( 'EARTH', 'GM', 1 );
+
+#mu = 398600.441E9 # Gravitational parameter for Earth [m^3/s^2]
+mu = muE[1][0] * 1e9
+
+spice.kclear
 
 CovertedKep = convertCartesianToKepler(S_bar,mu,True,True)    # Position arguments are passed
 
@@ -41,3 +56,4 @@ Kepler = np.array([a,e,i,RAAN,omega,M])
 
 ConvertedCarte = convertKeplerToCartesian(Kepler,mu,1,isInputInDegree = True, isPrint=True)
 ConvertedCarte2 = convertKeplerToCartesian(Kepler,mu,7,isInputInDegree = True, isPrint=True)
+
